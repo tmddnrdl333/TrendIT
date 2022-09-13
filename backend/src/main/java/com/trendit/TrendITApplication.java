43a747b4c6d8fcca5bfd43f8137d30feb8452230
WOOSIEUN <ramtk6726@naver.com>
2022-09-13 11:40:36 +0900

    [S07P22A301-72] feat: 백엔드 구조 세팅

---
+++ backend/src/main/java/com/trendit/TrendITApplication.java
@@ -0,0 +1,32 @@
+package com.trendit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import java.nio.charset.StandardCharsets;
+
+@SpringBootApplication
+@EnableScheduling
+public class TrendITApplication {
+	public static void main(String[] args) {
+        SpringApplication.run(TrendITApplication.class, args);
+    }
+
+    @Bean
+    public HttpMessageConverter<String> responseBodyConverter() {
+        return new StringHttpMessageConverter(StandardCharsets.UTF_8);
+    }
+
+    @Bean
+    public CharacterEncodingFilter characterEncodingFilter() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return characterEncodingFilter;
+    }
+}
