86a7685ec159a2c5440b4dfe58fa18ddb8a6ad9f
김혜준 <kimhj0107@naver.com>
2022-09-14 16:15:05 +0900

    S07P22A301-62/ chore: JPA entity error solve

--- backend/src/main/java/com/trendit/TrendITApplication.java
+++ backend/src/main/java/com/trendit/TrendITApplication.java
@@ -3,6 +3,7 @@ package com.trendit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -12,6 +13,7 @@ import java.nio.charset.StandardCharsets;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableJpaAuditing
 public class TrendITApplication {
 	public static void main(String[] args) {
         SpringApplication.run(TrendITApplication.class, args);
