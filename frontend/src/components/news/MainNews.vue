b8393115341fb7c2d2e8b018bec3378e717a307a
ygeum2@naver.com <ygeum2@naver.com>
2022-09-20 11:10:43 +0900

    [S07P22A301-119] feat: news page vue layout

---
+++ frontend/src/components/news/MainNews.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="keyword-analyze q-pa-lg">
+    <div>최신뉴스</div>
+  </div>
+</template>
+
+<style scoped>
+.keyword-analyze {
+  height: 480px;
+  width: 1200px;
+  background-color: #ffffff;
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+</style>
---
+++ frontend/src/components/news/SubNews.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="keyword-analyze q-pa-lg">
+    <div>서브뉴스</div>
+  </div>
+</template>
+
+<style scoped>
+.keyword-analyze {
+  height: 1200px;
+  width: 1200px;
+  background-color: #ffffff;
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+</style>
