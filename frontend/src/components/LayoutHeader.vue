b55ca30f6fb00dfb8334196655957ec90029e7dd
ygeum2@naver.com <ygeum2@naver.com>
2022-09-20 12:01:34 +0900

    [S07P22A301-119] feat: main page vue layout

--- frontend/src/components/LayoutHeader.vue
+++ frontend/src/components/LayoutHeader.vue
@@ -5,5 +5,6 @@
     <q-btn flat label="기업 리스트" href="company" />
     <q-btn flat label="최신 뉴스" href="news" />
     <q-btn flat label="서비스 소개" href="about" />
+    <q-btn flat label="TEST(임시)" href="test" />
   </q-toolbar>
 </template>
--- frontend/src/pages/MainView.vue
+++ frontend/src/pages/MainView.vue
@@ -1,18 +1,24 @@
 <template>
   <q-page class="flex flex-center">
     <!-- 메인 페이지에 넣을 컴포넌트들을 채워주삼 -->
-    <api-test />
+    <today-trend />
+    <today-issue />
+    <trend-rank />
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from "vue";
-import ApiTest from "../components/main/ApiTest.vue";
+import TodayTrend from "../components/main/TodayTrend.vue";
+import TodayIssue from "../components/main/TodayIssue.vue";
+import TrendRank from "../components/main/TrendRank.vue";
 
 export default defineComponent({
   name: "MainView",
   components: {
-    ApiTest,
+    TodayTrend,
+    TodayIssue,
+    TrendRank,
   },
 });
 </script>
