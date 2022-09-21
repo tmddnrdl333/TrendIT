bef548ac148dce7611b8ce58d738dccd0389a71e
pingu9 <djg4053@gmail.com>
2022-09-22 02:06:25 +0900

    [S07P22A301-107] chore: add comment

--- backend/src/main/java/com/trendit/common/util/RepositoryUtils.java
+++ backend/src/main/java/com/trendit/common/util/RepositoryUtils.java
@@ -25,6 +25,8 @@ public class RepositoryUtils {
         queryBuffer.append("and n.newsId in (select max(n.newsId) from KeywordHasNews kn join kn.news n group by kn.keyword.keywordId) ");
         queryBuffer.append("order by s.frequency desc");
 
+        /* TODO : dependant subquery가 발생하는 문제점 -> 더 나은 해결책? */
+
 //        SELECT s.frequency, k.keyword, n.news_id, n.headline FROM statistics_date as s JOIN keyword as k ON s.keyword_id = k.keyword_id
 //        join (SELECT kn.keyword_id, max(kn.news_id) as recent_news_id from keyword_has_news kn group by keyword_id) recent_news on s.keyword_id = recent_news.keyword_id
 //        JOIN news as n on n.news_id = recent_news.recent_news_id
