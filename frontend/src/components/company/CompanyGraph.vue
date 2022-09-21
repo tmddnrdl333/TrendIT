107950438f46aa5627825f60f911cacf50eab35e
정승욱 <tmddnrdl333@naver.com>
2022-09-21 15:55:14 +0900

    [S07P22A301-156] feat: CompanyView 페이지 작성

--- frontend/src/components/company/CompanyGraph.vue
+++ frontend/src/components/company/CompanyGraph.vue
@@ -1,23 +1,21 @@
 <template>
   <div class="company-graph q-pa-lg">
     <div>기업별 뉴스분석</div>
+    <q-separator inset />
     <CompanyBarChart />
-    <KeywordLineChart />
   </div>
 </template>
 
 <script>
 import CompanyBarChart from "../charts/CompanyBarChart.vue";
-import KeywordLineChart from "../charts/KeywordLineChart.vue";
 export default {
   setup() {},
-  components: { CompanyBarChart, KeywordLineChart },
+  components: { CompanyBarChart },
 };
 </script>
 
 <style scoped>
 .company-graph {
-  height: 450px;
   /* common */
   width: 1200px;
   background-color: #ffffff;
--- frontend/src/components/company/CompanyList.vue
+++ frontend/src/components/company/CompanyList.vue
@@ -1,8 +1,67 @@
 <template>
   <div class="company-list q-pa-lg">
     <div>기업 리스트</div>
+
+    <!-- 검색창 -->
+    <div class="q-px-md">
+      <q-skeleton type="rect" class="q-ma-md" width="800px" height="40px" />
+    </div>
+    <q-separator inset />
+    <div class="flex q-pa-lg justify-around">
+      <template v-for="(item, index) of company_list" :key="index">
+        <q-card class="company-card q-my-md">
+          <q-card-section>
+            {{ item.companyName }}
+          </q-card-section>
+          <q-separator />
+          <q-card-section>
+            <div>업종: {{ item.companyCategory }}</div>
+            <div>음음: {{ item.companyMainItem }}</div>
+            <div>링크: {{ item.companyLink }}</div>
+            <!-- <div>{{ item.keyword }}</div> -->
+          </q-card-section>
+        </q-card>
+      </template>
+    </div>
+    <div class="q-pa-lg flex flex-center">
+      <q-pagination v-model="current" :max="5" input />
+    </div>
   </div>
 </template>
+
+<script>
+import { companyListApi } from "boot/company.js";
+import { ref } from "vue";
+export default {
+  setup() {
+    const company_list = ref([{ companyName: "" }]);
+    const current = ref(1);
+
+    return {
+      company_list,
+      current,
+    };
+  },
+  created() {
+    this.loadCompanyList();
+  },
+  methods: {
+    async loadCompanyList() {
+      await companyListApi(
+        (response) => {
+          this.company_list = response.data.data;
+        },
+        () => console.warn("failed")
+      );
+    },
+  },
+  watch: {
+    current: function () {
+      this.loadCompanyList();
+    },
+  },
+};
+</script>
 
 <style scoped>
 .company-list {
@@ -11,5 +70,10 @@
   background-color: #ffffff;
   margin-top: 10px;
   margin-bottom: 10px;
+}
+.company-card {
+  height: 200px;
+  width: 230px;
+  padding: 15px;
 }
 </style>
