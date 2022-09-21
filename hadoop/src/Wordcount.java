767a1d254545fe1da6debc4fbf9d0cc6a08aa469
WOOSIEUN <ramtk6726@naver.com>
2022-09-21 10:40:53 +0900

    [S07P22A301-115] doc: Update README.md

--- hadoop/src/Wordcount.java
+++ hadoop/src/Wordcount.java
@@ -19,17 +19,11 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
 public class Wordcount {
-	/* 
-	Object, Text : input key-value pair type (always same (to get a line of input file))
-	Text, IntWritable : output key-value pair type
-	*/
 	public static class TokenizerMapper	extends Mapper<Object,Text,Text,IntWritable> {
 
-		// variable declairations
 		private final static IntWritable one = new IntWritable(1);
 		private Text word = new Text();
 
-		// map function (Context -> fixed parameter)
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 			StringTokenizer itr = new StringTokenizer(value.toString());
 			while ( itr.hasMoreTokens() ) {
@@ -39,17 +33,10 @@ public class Wordcount {
 		}
 	}
 
-	/*
-	Text, IntWritable : input key type and the value type of input value list
-	Text, IntWritable : output key-value pair type
-	*/
 	public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
 
-		// variables
 		private IntWritable result = new IntWritable();
 
-		// key : a disticnt word
-		// values :  Iterable type (data list)
 		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
 			int sum = 0;
 			for ( IntWritable val : values ) 
@@ -59,8 +46,6 @@ public class Wordcount {
 		}
 	}
 
-
-	/* Main function */
 	public static void main(String[] args) throws Exception {
 		Configuration conf = new Configuration();
 		String[] otherArgs = new GenericOptionsParser(conf,args).getRemainingArgs();
