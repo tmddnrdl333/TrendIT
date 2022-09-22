37f7252903b1e355984d018eb16f3a987088206f
ygeum2@naver.com <ygeum2@naver.com>
2022-09-22 15:49:07 +0900

    [S07P22A301-121] feat: MainNews in NewsPage 진행중

--- frontend/src/boot/getnewsByOptions.js
+++ frontend/src/boot/getnewsByOptions.js
@@ -1,8 +1,8 @@
 import { api } from "./axios.js";
 
-async function dateNewsAgencySearchApi(newsDate, newsAgency, success, fail) {
+async function getnewsByOptionsApi(newsDate, newsAgency, success, fail) {
   // await api.get("/news/latest").then(success).catch(fail).finally;
   console.log(newsDate, newsAgency, success, fail);
 }
 
-export { dateNewsAgencySearchApi };
+export { getnewsByOptionsApi };
