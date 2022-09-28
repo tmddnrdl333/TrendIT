e11f04a38a2c60be7cfc65e2b598886af1a06cab
WOOSIEUN <ramtk6726@naver.com>
2022-09-28 11:28:41 +0900

    [S07P22A301-172] feat: Implement user dictionary util

--- python/utils/user_dictionary_util.py
+++ python/utils/user_dictionary_util.py
@@ -1,10 +1,29 @@
-data= []
-while True:
-    line = input()
-    if(line == ""): break
-    data.append(line)
+from konlpy.tag import Komoran
 
-user_dict_file = open("../user_data/user_dictionary.txt", 'a', encoding='utf8')
-for line in data:
-    user_dict_file.write(line + "\tNNP\n")
-user_dict_file.close()
+def addKeyword():
+    data= []
+    while True:
+        line = input()
+        if line == "" : break
+        data.append(line)
+
+    user_dict_file = open("../user_data/user_dictionary.txt", 'a', encoding='utf8')
+    for line in data:
+        user_dict_file.write(line + "\tNNP\n")
+    user_dict_file.close()
+
+def morpho_analysis():
+    komoran = Komoran(userdic="../user_data/user_dictionary.txt")
+
+    while True:
+        line = input()
+        if line == "": break
+        print(komoran.nouns(line))
+
+while True:
+    print("1. 사용자 사전 단어 추가\n2. 형태소 분석\n3. 종료")
+    line = input()
+    if line == "": break
+    elif line == "1": addKeyword()
+    elif line == "2": morpho_analysis()
+    elif line == "3": break
