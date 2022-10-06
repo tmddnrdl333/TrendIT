8bcff0d86a06a607fea55242b436ba2577b2fd76
정승욱 <tmddnrdl333@naver.com>
2022-10-06 16:32:03 +0900

    [S07P22A301-264] design: title변경, log override

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -185,7 +185,7 @@ module.exports = configure(function (/* ctx */) {
       builder: {
         // https://www.electron.build/configuration/configuration
 
-        appId: "frontend",
+        appId: "TrendIT",
       },
     },
 
