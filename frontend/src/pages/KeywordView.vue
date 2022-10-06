eb53f9e76196e8fdd378635b9b747bb17a9263bc
정승욱 <tmddnrdl333@naver.com>
2022-10-06 10:17:56 +0900

    [S07P22A301-264] design: 키워드분석 페이지 디자인 수정

--- frontend/src/pages/KeywordView.vue
+++ frontend/src/pages/KeywordView.vue
@@ -1,31 +1,17 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page>
     <keyword-comp />
-    <keyword-create />
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from "vue";
 import KeywordComp from "../components/keyword/KeywordComp.vue";
-import KeywordCreate from "../components/keyword/result/KeywordCreate.vue";
 
 export default defineComponent({
   name: "KeywordView",
   components: {
     KeywordComp,
-    KeywordCreate,
   },
-
-  data() {
-    return {
-      sampleData: "",
-    };
-  },
-  setup() {},
-  created() {},
-  mounted() {},
-  unmounted() {},
-  methods: {},
 });
 </script>
